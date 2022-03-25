fc6906f82989724b03823aae8fee29bd52726bf5
winkeyt <80002036+winkeyt@users.noreply.github.com>
2022-03-25 16:39:03 +0000

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package pl.vistula;
+public class Calculator implements SimpleCalculator
+
+    public double add(double first, double second){
+        return 
+        solution = first + second;
+        System.out.println("Print solution: " + solution );
+    }
+      
+    public double sub(double first, double second){
+        return 
+        solution = first - second;
+        System.out.println("Print solution: " + solution );
+    }
+
+    public double mul(double first, double second){
+        return solution = first * second;
+        System.out.println("Print solution: " + solution );
+    }
+
+    public double div(double first, double second){
+        return solution = first / second;
+        System.out.println("Print solution: " + solution );
+    }
+
+}
