c6fb3eeaff7253cb61797e382cd121e55f72bf63
winkeyt <80002036+winkeyt@users.noreply.github.com>
2022-03-25 16:00:54 +0000

    Create HelloWorld in java

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World. My name: Thai Thanh Nguyen. Id: 50094"
+                           }
+}
