1534dbfa881e20e68190a43ba78a507d10daa56d
winkeyt <80002036+winkeyt@users.noreply.github.com>
2022-03-28 18:33:06 +0100

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,28 +1,60 @@
 package pl.vistula;
 public class Calculator implements SimpleCalculator
 
-    public double add(double first, double second){
-        return 
-        solution = first + second;
-        System.out.println("Print solution: " + solution );
-    }
-      
-    public double sub(double first, double second){
-        return 
-        solution = first - second;
-        System.out.println("Print solution: " + solution );
-    }
 
-    public double mul(double first, double second){
-        return 
-        solution = first * second;
-        System.out.println("Print solution: " + solution );
-    }
+        public static void main(String[] args)
+        {
+        Scanner in = new Scanner(System.in);
+            double a, b, c = 0.0;
 
-    public double div(double first, double second){
-        return       
-        solutionssolution = first / second;
-        System.out.println("Print solution: " + solution );
-    }
+        System.out.print("Enter a: ");
+        a = in.nextDouble();
+        System.out.print("Enter b: ");
+        b = in.nextDouble();
+    
+    System.out.print("The operation  " +
+                "\n1.Addition" +
+        		"\n2.Subtraction" +
+        		"\n3.Multiplication" +
+        		"\n4.Division" +
+        		"\n5.Power" +
+        		"\n#Please enter the number of operation \n");
+        double x = in.nextDouble();
+        double add = 1;
+        double sub = 2;
+        double mul= 3;
+        double div = 4 ;
+        double power = 5;
 
+            public double add(double a, double b){
+                x = add;
+                c = a + b;
+                System.out.println("Print solution: " + c );
+            }
+
+            public double sub(double a, double b){
+                x = sub;
+                c = a - b;
+                System.out.println("Print solution: " + c );
+            }
+
+            public double mul(double a, double b){
+                x = mul;
+                c = a * b;
+                System.out.println("Print solution: " + c );
+            }
+
+            public double div(double a, double b){
+                x = div;     
+                c = a / b;
+                if ( b = 0 )
+                    {
+                    System.out.println("Can not calculator " );
+                    }
+                else ( b > 0 )
+                    {
+                    System.out.println("Print solution: " + c );
+                    }
+            }
+        }
 }
